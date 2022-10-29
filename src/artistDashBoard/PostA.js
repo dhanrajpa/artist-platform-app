c7ccda584c523daa4f4b7c524a86c394185f22fa
dhanrajpa <dhanrajpa@cybage.com>
2022-10-29 20:47:48 +0530

    Post card Ui updated

--- src/artistDashBoard/PostA.js
+++ src/artistDashBoard/PostA.js
@@ -9,47 +9,97 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
 import Favorite from '@mui/icons-material/Favorite';
+import ChatRoundedIcon from '@mui/icons-material/ChatRounded';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import { Box, Card, CardHeader, Checkbox, Button } from '@mui/material'
-import ProfilePicGoku from '../images/ProfilePicGoku.jpg'
+import { Box, Card, CardHeader, Checkbox, Button, Tooltip } from '@mui/material'
+import ProfilePicGoku from '../images/tom-cruise-secret-life.webp'
+import { fontFamily, fontStyle } from '@mui/system';
+import '../Scss/ArtistDashboard/PostA/PostA.css'
+import { Link } from 'react-router-dom';
+
+
+
+
+
 
 const PostA = (props) => {
 
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+
     const img = { props }
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(!anchorElNav);
+    };
 
     return (
         <>
-            <Card sx={{ margin: 2 }}>
+            <Card sx={{ margin: 2, boxShadow: 0 }}>
 
-                <IconButton gutterBottom variant="h6" sx={{ float: "left", ml: "0.3rem" }} letterSpacing={2} component="div">
+                <IconButton variant="h6" sx={{ float: "left", ml: "0.3rem", mt: "0.3rem" }} letterSpacing={2} component="div">
                     <Avatar alt="Goku" src={ProfilePicGoku} sx={{ width: 32, height: 32 }} />
                 </IconButton>
 
-                <Typography gutterBottom variant="h6" color="inherit" pt={1} sx={{ float: "left", ml: "0.3rem" }} letterSpacing={1} component="div">
-                    Kakarot
+                <Typography variant="h6" color="inherit" pt={1} sx={{ float: "left", mt: "0.3rem" }} letterSpacing={1} component="div">
+                    <Button sx={{ fontWeight: 'bold', pb: 0, pt: 0, display: 'inline-flex', color: '#1F1F1F', TextDecoder: 'none' }}>Kakarot</Button>
                 </Typography>
 
                 <CardMedia
                     component="img"
-                    height="20%"
+                    // height="10%"
                     image={ProfilePicGoku}
-                    sx={{ height: '30rem' }}
+                    sx={{ height: '30rem', boxShadow: 1 }}
                 />
 
-                <CardContent>
-                    <Typography variant="body3" color="text.secondary">
-                        {/* {imgs.props.imgs.imgdesc} */}
+                <CardActions disableSpacing sx={{ position: 'relative', pt: 0, pb: 0 }} >
+                    <Box flexDirection="column" sx={{ display: 'flex', pb: 0 }}>
+                        <Box flexDirection="row" sx={{ display: 'flex', pb: 0 }} >
+                            <Tooltip title="Like" >
+                                <Checkbox uncheckedIcon={<FavoriteBorder fontSize='medium' sx={{ mb: 0, mt: 0, ml: 0 }} />} icon={<FavoriteBorder fontSize='medium' sx={{ mb: 0, mt: 0, ml: 0 }} />} checkedIcon={<Favorite sx={{ color: "red", mb: 0, transform: "scale(1.2)", }} />} />
+                            </Tooltip>
+                            <Tooltip title="See Comments" >
+                                <IconButton aria-label="add to favorites" sx={{ mb: 0, mt: 0, ml: 2 }} onClick={handleOpenNavMenu} >
+                                    <ChatRoundedIcon fontSize='medium' />
+                                </IconButton>
+                            </Tooltip>
+                        </Box>
+                        <Typography variant="body3" color="#1F1F1F" fontWeight='fontWeightMedium' id="Like-content" href="/" >
+                            1 Like
+                        </Typography>
+                    </Box>
+                </CardActions>
+
+                <CardContent id="Card-content">
+                    {/* <Typography variant="body2" color="#1F1F1F" sx={{ fontWeight: 'bold', pb: 0, display: 'inline-flex', height: 1 }}> */}
+                    <Button component={Link} to='#' sx={{ fontWeight: 'bold', pr: 1, pb: 0, pt: 0, pl: 0, color: '#1F1F1F', }} id="User-Name">Kakarot</Button>
+                    {/* </Typography> */}
+                    <Typography variant="body2" color="text.secondary" id="content-bio" align="justify" sx={{ pb: 1, pr: 1 }}>
+                        the 'color' props to TypgraphyYou dont need to seperately pass the 'color' props to Typgraphy 'color' props to Typgraphy
+                        the 'color' props to TypgraphyYou dont need to seperately pass the 'color' props to Typgraphy 'color' props to Typgraphy
+
                     </Typography>
                 </CardContent>
 
-                <CardActions disableSpacing>
-                    <IconButton aria-label="add to favorites">
-                        <Checkbox icon={<FavoriteBorder />} checkedIcon={<Favorite sx={{ color: "red", mb: 0 }} />} />
-                    </IconButton>
-                    <Typography variant="body3" color="text.secondary">
-                        <Button> Comments </Button>
+                {/* <Box id="Card-content">
+                    <Button sx={{ pb: 0, pl: 0, pt: 0, display: 'inline-flex', height: 1 }}>
+                        <Typography variant="body3" color="#1F1F1F" fontWeight='fontWeightMedium' id="Like-content" >
+                            1 Like
+                        </Typography>
+                    </Button>
+                    <Typography variant="body2" color="text.secondary" pl={1} pr={2} align="justify" id="content-bio" >
+                        Bi You dont need to sepera You dont need to seperately pass the 'color' props to TypgraphyYou dont need to seperately pass the 'color' props to TypgraphyYou dont need to seperately pass the 'color' props to Typgraphy
                     </Typography>
-                </CardActions>
+                </Box> */}
+
+
+
+
+
+
+
+
+
+
             </Card>
         </>
     )
